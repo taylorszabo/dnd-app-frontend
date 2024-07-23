eda45c3415e9363b7f538660ad8ab2395af2cb32
easyseven-vehikl <132284601+easyseven-vehikl@users.noreply.github.com>
2024-07-23 16:56:19 -0400

    Add search and style CR color

--- components/MonsterList.js
+++ components/MonsterList.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {ActivityIndicator, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import axios from 'axios';
 
 const MonsterList = () => {
@@ -7,6 +7,7 @@ const MonsterList = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [expandedId, setExpandedId] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
         axios.get('http://127.0.0.1:8000/api/monsters')
@@ -24,6 +25,28 @@ const MonsterList = () => {
         setExpandedId(expandedId === id ? null : id);
     };
 
+    const handleSearch = (text) => {
+        if (text) {
+            setSearchQuery(text);
+        }
+    };
+
+    const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    const getCrStyle = (cr) => {
+        if (cr >= 1 && cr <= 9) {
+            return styles.crGreen;
+        } else if (cr >= 10 && cr <= 19) {
+            return styles.crYellow;
+        } else if (cr >= 20 && cr <= 30) {
+            return styles.crRed;
+        }
+        return styles.crDefault;
+    };
+
+
     if (loading) {
         return <ActivityIndicator size="large" color="#0000ff"/>;
     }
@@ -34,18 +57,25 @@ const MonsterList = () => {
 
     return (
         <View style={styles.container}>
+            <TextInput
+                style={styles.searchInput}
+                placeholder="Search"
+                placeholderTextColor="#ccc"
+                value={searchQuery}
+                onChangeText={handleSearch}
+            />
             <View style={styles.headerRow}>
                 <Text style={styles.headerText}>Monster</Text>
                 <Text style={styles.headerText}>CR</Text>
             </View>
         <FlatList
-            data={monsters}
+            data={filteredMonsters}
             keyExtractor={item => item.id.toString()}
             renderItem={({item, index}) => (
                 <TouchableOpacity onPress={() => toggleExpand(item.id)}>
                     <View style={[styles.item, index % 2 === 0 ? styles.darkItem : styles.lightItem]}>
                         <View style={styles.row}>
-                            <View style={styles.column}>
+                            <View style={styles.titleColumn}>
                                 <Text style={styles.title}>{item.name}</Text>
                                 {expandedId === item.id && (
                                     <View>
@@ -54,8 +84,8 @@ const MonsterList = () => {
                                     </View>
                                 )}
                             </View>
-                            <View style={styles.column}>
-                                <Text style={styles.subtitle}>CR {item.cr}</Text>
+                            <View style={styles.subtitleColumn}>
+                                <Text style={[styles.subtitle, getCrStyle(item.cr)]}>{item.cr}</Text>
                                 {expandedId === item.id && (
                                     <View>
                                         <Text style={styles.detailText}>Size: {item.size}</Text>
@@ -77,6 +107,14 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#1E1E1E',
         flex: 1,
+    },
+    searchInput: {
+        height: 45,
+        borderRadius: 20,
+        paddingHorizontal: 10,
+        margin: 5,
+        color: '#fff',
+        backgroundColor: '#4C4C4C',
     },
     headerRow: {
         flexDirection: 'row',
@@ -104,8 +142,12 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
-    column: {
+    titleColumn: {
         flex: 1,
+    },
+    subtitleColumn: {
+        flex: 1,
+        paddingLeft: 40,
     },
     title: {
         fontSize: 18,
@@ -114,11 +156,24 @@ const styles = StyleSheet.create({
     },
     subtitle: {
         fontSize: 18,
-        color: '#fff',
+        color: '#27E607',
+        textAlign: "center",
     },
     detailText: {
         color: '#fff',
         marginTop: 5,
+    },
+    crGreen: {
+        color: 'lime',
+    },
+    crYellow: {
+        color: 'yellow',
+    },
+    crRed: {
+        color: 'red',
+    },
+    crDefault: {
+        color: '#fff',
     },
     errorText: {
         color: 'red',
